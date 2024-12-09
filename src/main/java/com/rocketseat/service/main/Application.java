ecae5a33cf1872fe03bb735c2039c08cbeb78175
vhraposo <victorhcraposo@gmail.com>
2024-12-09 11:03:20 -0300

    Aula 06

--- src/main/java/com/rocketseat/service/main/Application.java
+++ src/main/java/com/rocketseat/service/main/Application.java
@@ -2,8 +2,12 @@ package com.rocketseat.service.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
 public class Application {
 
 	public static void main(String[] args) {
